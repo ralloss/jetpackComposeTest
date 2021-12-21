7fad992eda93df624616df6679bc65602e970e3e
ralloss <kouzouloss@gmail.com>
2021-12-21 16:54:04 +0200

    finished chat demo ui tutorial

--- app/src/main/java/com/example/composetestapplication/MainActivity.kt
+++ app/src/main/java/com/example/composetestapplication/MainActivity.kt
@@ -3,11 +3,25 @@ package com.example.composetestapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetestapplication.ui.theme.ComposeTestApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +29,74 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeTestApplicationTheme {
-        Greeting("Android")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(8.dp)) {
+
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor:Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+
+            Text(
+                text = "${msg.author}",
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp, color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+                ) {
+
+                Text(
+                    text = "Message: ${msg.body}!",
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+
+
+                )
+            }
+
+        }
     }
+
 }
